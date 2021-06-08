2c1bc1dee8dbccfa016fbe1830116c63953f681c
Brunno Rafaell <lotsfu@gmail.com>
2021-06-07 21:29:58 -0300

    acrescentei msg de boas vindas

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -4,6 +4,8 @@ namespace Projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("TAMO JUNTO AQUI RAPAZ");
+            Console.ReadLine();
         }
     }
 }
